c69e27b83c442db7833fa5a36539ceecb43c10e5
Ki-Hyoun Kim <kihyoun@gmail.com>
2018-12-11 22:01:29 +0100

    Update day1.php

--- day1.php
+++ day1.php
@@ -1,2 +1,25 @@
+<?php
+$file = file_get_contents("freq.txt");
+$numbers = explode("\n", $file);
 
-$file = file_get_contents(frequency.txt);
+$sum = 0;
+$strippedlist = array(0);
+
+while(true) {
+	foreach ($numbers as $key => $number) {
+		$stripped = trim(intval($number));	
+		$sum += $stripped;
+			
+		if (in_array($sum, $strippedlist)) {
+				echo $sum . 'is the first double';
+				exit;
+		}
+		
+		$strippedlist[] = $sum;	
+	}
+}
+
+//echo $sum;
+//if (in_array(56, $strippedlist)) echo "FOUND";
+
+?>
