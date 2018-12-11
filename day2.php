65fabb8f74b1a19750130dbcc5792d5c61d72862
Ki-Hyoun Kim <kihyoun@gmail.com>
2018-12-11 22:03:53 +0100

    Create day2.php

---
+++ day2.php
@@ -0,0 +1,45 @@
+<?php
+$file = file_get_contents("day3.txt");
+$words = explode("\n", $file);
+$two = 0;
+$three = 0;
+foreach ($words as $word) {
+	$letters = str_split(trim($word));
+	sort($letters);
+	
+	$isThree=false;
+	$isTwo=false;
+	
+	while (count($letters) > 1) {
+		// Vergleiche "cur" mit restlichen buchstaben
+		$cur = array_pop($letters);
+		$count_cur = 1;
+		
+		foreach ($letters as $k => $letter) {
+			if ($cur == $letter) {
+				$count_cur++;
+				unset($letters[$k]);
+			}
+		}		
+		
+		if ($count_cur == 3 && !$isThree) {
+			$three++;
+			$isThree = true;
+		}
+		
+		if ($count_cur == 2 && !$isTwo) {
+			$two++;
+			$isTwo = true;
+		}
+		
+		if ($isTwo && $isThree) break;
+		
+		$tmp = array_values($letters);
+		$letters = $tmp;		
+	}	
+}
+
+$code = $two * $three;
+echo "Code " . $code;
+
+?>
